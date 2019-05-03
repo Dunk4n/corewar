88e666597a104516ceb6997d9c414528a62fa303
Nicolas Duchesne <nicolasduchesne884@gmail.com>
2019-05-03 17:34:04 +0200

    try to fix coding style error

--- asm/include/op.h
+++ asm/include/op.h
@@ -11,8 +11,8 @@
 # include <unistd.h>
 
 # define MEM_SIZE (6*1024)
-# define IDX_MOD 512   /* modulo of the index < */
-# define MAX_ARGS_NUMBER 4     /* this may not be changed 2^*IND_SIZE */
+# define IDX_MOD 512
+# define MAX_ARGS_NUMBER 4
 
 # define COMMENT_CHAR '#'
 # define LABEL_CHAR ':'
@@ -24,24 +24,15 @@
 # define NAME_CMD_STRING ".name"
 # define COMMENT_CMD_STRING ".comment"
 
-/*
-** regs
-*/
-
-# define REG_NUMBER 16              /* r1 <--> rx */
-
-/*
-**
-*/
+# define REG_NUMBER 16
 
 typedef char    args_type_t;
 
-# define T_REG 1       /* register */
-# define T_DIR 2       /* direct  (ld  #1,r1  put 1 into r1) */
-# define T_IND 4       /* indirect always relative
-                                   ( ld 1,r1 put what's in the address (1+pc)
-                                   into r1 (4 bytes )) */
-# define T_LAB 8       /* LABEL */
+# define T_REG 1
+# define T_DIR 2
+# define T_IND 4
+
+# define T_LAB 8
 
 struct  op_s
 {
@@ -55,29 +46,20 @@ struct  op_s
 
 typedef struct op_s     op_t;
 
-/*
-** size (in bytes)
-*/
 # define IND_SIZE 2
 # define DIR_SIZE 4
 # define REG_SIZE DIR_SIZE
 
-/*
-** op_tab
-*/
-
 const   op_t    op_tab[17];
 
-/*
-** header
-*/
 # define PROG_NAME_LENGTH 128
 # define COMMENT_LENGTH 2048
+
+# define COREWAR_EXEC_MAGIC 0xea83f3
 
 struct header_s
 {
    int  magic;
-# define COREWAR_EXEC_MAGIC 0xea83f3        /* why not */
    char prog_name[PROG_NAME_LENGTH + 1];
    int  prog_size;
    char comment[COMMENT_LENGTH + 1];
@@ -85,10 +67,7 @@ struct header_s
 
 typedef struct header_s header_t;
 
-/*
-** live
-*/
-# define CYCLE_TO_DIE 1536    /* number of cycle before beig declared dead */
+# define CYCLE_TO_DIE 1536
 # define CYCLE_DELTA 5
 # define NBR_LIVE 40
 
