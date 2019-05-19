2f7b5bdd36f7285c39e229d55b57643a6af420e4
Alexandre.Wagner <alexandre.wagner@alexandre.wagner>
2019-05-19 17:17:18 +0200

    correction -h norm

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -8,20 +8,22 @@
 #include <stdlib.h>
 #include "corwar.h"
 
-int     flag_h(char *name)
+int    flag_h(char *nam)
 {
-    my_printf("USAGE\n\t./%s [-dump nbr_cycle] [[-n prog_number] ", name);
-    my_putstr("[-a load_address] prog_name] ...\n\n");
-    my_putstr("DESCRIPTION\n\t-dump nbr_cycle\tdumps the memory after the \
-nbr_cycle execution (if the round isn’t\n\t\t\talready over) with the \
-following format:  32 bytes/line in\n\t\t\thexadecimal (A0BCDEFE1DD3...)\n");
-    my_putstr("\t-n prog_number\tsets the next program’s number.  By default, \
-the first free number\n\t\t\tin the parameter order\n");
-    my_putstr("\t-a load_address\tsets the next program’s loading address. \
-When no address is\n\t\t\tspecified, optimize the addresses so that the \
-processes are as far\n\t\t\taway from each other as possible.  The addresses \
-are MEM_SIZE modulo\n");
-    return (0);
+    my_putstr("USAGE\n");
+    my_printf("\t%s [-dump nbr_cycle] [[-n prog_number] [-a load_adress]", nam);
+    my_putstr(" prog_name] ...\n\n");
+    my_putstr("DESCRIPTION\n");
+    my_putstr("\t-dump nbr_cycle dumps the memory after the nbr_cycle executio");
+    my_putstr("n (if the round isn't\n\t\t\talready over) with the following");
+    my_putstr(" format: 32 bytes/line in\n\t\t\thexadecimal (AOBCDEFE1DD3...)");
+    my_putstr("\n\t-n prog_number  sets the next program's number. By default,");
+    my_putstr(" the first free number\n\t\t\tin the parameter order\n");
+    my_putstr("\t-a load_adress  sets the next program's loading adress. When");
+    my_putstr(" no adress is\n\t\t\tspecified, optimize the addresses so that ");
+    my_putstr("the processes are as far\n\t\t\taway from each other as");
+    my_putstr(" possible. The addresses are MEM_SIZE modulo\n");
+    return 0;
 }
 
 void    set_value(corewar_t *core)
